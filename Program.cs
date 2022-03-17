374278804f1cde9c4b56361707246746d8df543d
sonalieva <708v8lg@code.edu.az>
2022-03-17 13:50:40 +0400

    created methodtask file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace methodtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40 };
+            int max = largest(arr);
+            Console.WriteLine(max);
+
+
+            static int largest(int[] arr)
+            {
+                int max = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                    if (arr[i] > max)
+                        max = arr[i];
+                return max;
+
+            }
+        }
+    }
+}
